aeb2a8808b567cb3d2376d9ecd75885b23c0d52d
ChaitanyaPimpare <chaitanyapimpare2003@gmail.com>
2025-03-31 18:04:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -102,4 +102,5 @@ if __name__ == '__main__':
     with app.app_context():    
       db.create_all()
 
-    app.run(debug=True)
+  port = int(os.environ.get("PORT", 8080))  # Default to 8080 if PORT is not set
+    app.run(host="0.0.0.0", port=port, debug=True)
